ae2037d529645374ac3c271fad36e0a8c5e30a80
sarthakshishodia20 <sarthakshishodia2004@gmail.com>
2026-09-17 12:50:47 +0530

    changesMade

--- nuzio_flutter/lib/core/constants.dart
+++ nuzio_flutter/lib/core/constants.dart
@@ -4,6 +4,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 // Web (Chrome): localhost
 // Android emulator: 10.0.2.2 (maps to host machine localhost)
 // Physical device: your LAN IP e.g. 192.168.1.x
-const String kBaseUrl = kIsWeb
-    ? 'http://localhost:3000/api'
-    : 'https://nuzio-api-sarthak.loca.lt/api';
+// Production Render backend API URL
+const String kBaseUrl = 'https://nuzio-backend.onrender.com/api';
+
